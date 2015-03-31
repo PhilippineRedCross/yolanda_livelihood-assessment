785182e335838fd1832872d6f444af5cb4fdf743
danbjoseph <danbjoseph@gmail.com>
2015-04-01 09:17:38 +1100

    include round 2 survey analysis

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@
 
 // global variables
 // ================
-var surveyData = [];
-var filteredData = [];
+var surveyDataBaseline = [];
+var surveyDataEndline = [];
+
+var filteredDataBaseline = [];
+var filteredDataEndline = [];
+
 var surveyQuestions = [];
 var answers = ["agree_strongly", "agree", "neither", "disagree", "disagree_strongly", "unsure", "na"];
-var summedResponses = [];
-var totalCount = 0;
+
+var summedResponsesBaseline = [];
+var summedResponsesEndline = [];
+
+var totalCountBaseline = 0;
+var totalCountEndline = 0;
+
 
 var adminLookup = {
 	"PH060400000" :	"Aklan",
@@ -34,43 +43,67 @@ var noDecimal = d3.format(".0f");
 // ==============
 
 // get CSV files
-function getData(){
-  d3.csv("data/HLA-SurveyData.csv", function(data){ 
-    surveyData = data;
+function getDataBaseline(){
+  d3.csv("data/HLA-SurveyDataBaseline.csv", function(data){
+    surveyDataBaseline = data;
+    getDataEndline();
+  });
+}
+
+function getDataEndline(){
+  d3.csv("data/HLA-SurveyDataEndline.csv", function(data){
+    surveyDataEndline = data;
     getQuestions();
   });
 }
 
 function getQuestions(){
-  d3.csv("data/SurveyQuestions.csv", function(data){ 
+  d3.csv("data/SurveyQuestions.csv", function(data){
     surveyQuestions = data;
     buildHtml();
   });
 }
 
 function buildHtml() {
-	totalCount = surveyData.length;
-	$("#surveyCount").html(totalCount);
+	totalCountBaseline = surveyDataBaseline.length;
+	$("#surveyCountBaseline").html(totalCountBaseline);
+
+	totalCountEndline = surveyDataEndline.length;
+	$("#surveyCountEndline").html(totalCountEndline);
+
 	$.each(surveyQuestions, function(index, question){
 		var sectionId = '#' + question.id;
 		var questionHtml = '<div id="' + question.name + '" class="question-block">' +
-			'<h5>' + question.label +  
+			'<h5>' + question.label +
 			((question.hint !== "none") ? '<br><small>' + question.hint + '</small>' : '') +
-			'<br><span class="text-tagalog">' + question["label-tagalog"] + '</span>' + 
+			'<br><span class="text-tagalog">' + question["label-tagalog"] + '</span>' +
 			((question["hint-tagalog"] !== "none") ? '<br><small><span class="text-tagalog">' +
 			question["hint-tagalog"] + '</span>' + '</small>' : '') +
 
 			'</h5><div class="responsesBar">' +
 	          '<svg width="100%" height="30">' +
-	            '<rect class="response-bar na" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar unsure" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar disagree_strongly" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar disagree" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar neither" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar agree" y="0" height="100%" ></rect>' +
-	            '<rect class="response-bar agree_strongly" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline na" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline unsure" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline disagree_strongly" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline disagree" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline neither" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline agree" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-baseline agree_strongly" y="0" height="100%" ></rect>' +
 	          '</svg>' +
-			'</div>';
+			'</div>'+
+			'<div class="responsesBar responsesEndline">' +
+	          '<svg width="100%" height="30">' +
+	            '<rect class="response-bar response-endline na" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline unsure" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline disagree_strongly" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline disagree" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline neither" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline agree" y="0" height="100%" ></rect>' +
+	            '<rect class="response-bar response-endline agree_strongly" y="0" height="100%" ></rect>' +
+	          '</svg>' +
+			'</div>'
+
+			;
 		$(sectionId).append(questionHtml);
 		var questionId = '#' + question.name;
 	});
@@ -79,7 +112,20 @@ function buildHtml() {
 
 function buildProvinceDropdown() {
   var provinceList = [];
-  $.each(surveyData, function(index, survey){
+	// baseline
+	// ========
+  $.each(surveyDataBaseline, function(index, survey){
+    var thisProvincePcode = survey["municipality"].slice(0,6) + "00000";
+    var thisProvince = adminLookup[thisProvincePcode];
+    survey["provinceName"] = thisProvince;
+    if(thisProvince == undefined){console.log(thisProvincePcode);}
+    if($.inArray(thisProvince, provinceList) === -1){
+      provinceList.push(thisProvince);
+    }
+  });
+	// endline
+	// ========
+	$.each(surveyDataEndline, function(index, survey){
     var thisProvincePcode = survey["municipality"].slice(0,6) + "00000";
     var thisProvince = adminLookup[thisProvincePcode];
     survey["provinceName"] = thisProvince;
@@ -89,37 +135,49 @@ function buildProvinceDropdown() {
     }
   });
   // sort so that the regions appear in alphabetical order in dropdown
-  provinceList = provinceList.sort(); 
-  // create item elements in dropdown list   
+  provinceList = provinceList.sort();
+  // create item elements in dropdown list
   for(var i = 0; i < provinceList.length; i++) {
       var item = provinceList[i];
       var listItemHtml = '<li><a href="#" onClick="provinceSelect(' +"'"+ item +"'"+ '); return false;">' + item + "</li>"
-      $('#dropdown-menu-province').append(listItemHtml);       
+      $('#dropdown-menu-province').append(listItemHtml);
   }
   provinceSelect("ALL");
 }
 
 function provinceSelect(selection){
-	filteredData = [];
+	filteredDataBaseline = [];
+	filteredDataEndline = [];
 	if(selection == "ALL"){
 		$("#selected-admin-label").html("All surveyed provinces");
-		filteredData = surveyData;
+		filteredDataBaseline = surveyDataBaseline;
+		filteredDataEndline = surveyDataEndline;
 
 	} else {
-		$.each(surveyData, function(index, survey){
-			$("#selected-admin-label").html(selection);
+		$("#selected-admin-label").html(selection);
+		$.each(surveyDataBaseline, function(index, survey){
 			if(survey.provinceName == selection){
-				filteredData.push(survey);
+				filteredDataBaseline.push(survey);
+			}
+		});
+		$.each(surveyDataEndline, function(index, survey){
+			if(survey.provinceName == selection){
+				filteredDataEndline.push(survey);
 			}
 		});
 	}
-	$("#selected-survey-count").html(filteredData.length);
+	$("#selected-survey-count-baseline").html(filteredDataBaseline.length);
+	$("#selected-survey-count-endline").html(filteredDataEndline.length);
+
 	parseData();
 }
 
 
 function parseData() {
-	summedResponses = [];
+
+	// baseline
+	// ========
+	summedResponsesBaseline = [];
 	// count up responses by question (A01, A02, ...) and answer (strongly_agree, agree, ...)
 	$.each(surveyQuestions, function(questionIndex, question){
 		var questionObject = {};
@@ -128,34 +186,55 @@ function parseData() {
 			answerCountsObject[answer] = 0;
 		});
 		questionObject[question.name] = answerCountsObject;
-	
 
-		$.each(filteredData, function(responseIndex, response){
+		$.each(filteredDataBaseline, function(responseIndex, response){
 			var thisAnswer = response[question.name];
 			questionObject[question.name][thisAnswer] += 1;
 		});
-		summedResponses.push(questionObject);
-		// summedResponses = { {A01: {agree:52, agree_strongly:5, disagree:16, ...}},{A02: {agree:56; ...}}, ... }
+		summedResponsesBaseline.push(questionObject);
+		// summedResponsesBaseline = { {A01: {agree:52, agree_strongly:5, disagree:16, ...}},{A02: {agree:56; ...}}, ... }
 	});
+
+	// endline
+	// ========
+	summedResponsesEndline = [];
+	// count up responses by question (A01, A02, ...) and answer (strongly_agree, agree, ...)
+	$.each(surveyQuestions, function(questionIndex, question){
+		var questionObject = {};
+		var answerCountsObject = {};
+		$.each(answers, function(answerIndex, answer){
+			answerCountsObject[answer] = 0;
+		});
+		questionObject[question.name] = answerCountsObject;
+
+		$.each(filteredDataEndline, function(responseIndex, response){
+			var thisAnswer = response[question.name];
+			questionObject[question.name][thisAnswer] += 1;
+		});
+		summedResponsesEndline.push(questionObject);
+		// summedResponsesEndline = { {A01: {agree:52, agree_strongly:5, disagree:16, ...}},{A02: {agree:56; ...}}, ... }
+	});
+
 	graphData();
 }
 
 function graphData() {
 
-	
-	$.each(summedResponses, function(questionIndex, questionObject){
+	// baseline
+	// ========
+	$.each(summedResponsesBaseline, function(questionIndex, questionObject){
 		var questionDivId = '';
 		var thisQuestionData = {};
 
-		$.each(questionObject, function(questionIndex, questionData){ 
+		$.each(questionObject, function(questionIndex, questionData){
 			questionDivId = '#' + questionIndex; // "#A01", "#A02", ...
 			thisQuestionData = questionData;
 		});
-	
+
 		// calculate the percentage for each and add to the div as an html data attr for the tooltip
 		$.each(thisQuestionData, function(answerIndex, answerData){
-			thisQuestionData[answerIndex] = ( answerData / filteredData.length ) * 100;
-			var selector = questionDivId + " ." + answerIndex;
+			thisQuestionData[answerIndex] = ( answerData / filteredDataBaseline.length ) * 100;
+			var selector = questionDivId + " .response-baseline." + answerIndex;
 			var styledPercentage = noDecimal(thisQuestionData[answerIndex]) + "%";
 			$(selector).attr("data-percentage", styledPercentage);
 		});
@@ -164,122 +243,101 @@ function graphData() {
 		// calculate each width as its own percentage plus those to the left
 		var agree_strongly = thisQuestionData["agree_strongly"];
 		thisQuestionData["agree_strongly"] = agree_strongly.toString() + "%";
-		
+
 		var agree = agree_strongly + thisQuestionData["agree"];
 		thisQuestionData["agree"] = agree.toString() + "%";
-		
+
 		var neither = agree + thisQuestionData["neither"];
 		thisQuestionData["neither"] = neither.toString() + "%";
-		
+
 		var disagree = neither + thisQuestionData["disagree"];
 		thisQuestionData["disagree"] = disagree.toString() + "%";
-		
+
 		var disagree_strongly = disagree + thisQuestionData["disagree_strongly"];
 		thisQuestionData["disagree_strongly"] = disagree_strongly.toString() + "%";
-		
+
 		var unsure = disagree_strongly + thisQuestionData["unsure"];
 		thisQuestionData["unsure"] = unsure.toString() + "%";
-		
+
 		var na = unsure + thisQuestionData["na"];
 		thisQuestionData["na"] = na.toString() + "%";
-		
+
 		// use the calculated widths to adjust the rectangles
 		$.each(thisQuestionData, function(indexa, responseCategorya){
-			var selector = " ." + indexa;
+			var selector = " .response-baseline." + indexa;
 			d3.select(questionDivId).select(selector).transition().attr("width", responseCategorya);
 		});
-		
+
+	});
+
+	// endline
+	// ========
+	$.each(summedResponsesEndline, function(questionIndex, questionObject){
+		var questionDivId = '';
+		var thisQuestionData = {};
+
+		$.each(questionObject, function(questionIndex, questionData){
+			questionDivId = '#' + questionIndex; // "#A01", "#A02", ...
+			thisQuestionData = questionData;
+		});
+
+		// calculate the percentage for each and add to the div as an html data attr for the tooltip
+		$.each(thisQuestionData, function(answerIndex, answerData){
+			thisQuestionData[answerIndex] = ( answerData / filteredDataEndline.length ) * 100;
+			var selector = questionDivId + " .response-endline." + answerIndex;
+			var styledPercentage = noDecimal(thisQuestionData[answerIndex]) + "%";
+			$(selector).attr("data-percentage", styledPercentage);
+		});
+
+		// the viz is overlapping svg rectangle in the same category order
+		// calculate each width as its own percentage plus those to the left
+		var agree_strongly = thisQuestionData["agree_strongly"];
+		thisQuestionData["agree_strongly"] = agree_strongly.toString() + "%";
+
+		var agree = agree_strongly + thisQuestionData["agree"];
+		thisQuestionData["agree"] = agree.toString() + "%";
+
+		var neither = agree + thisQuestionData["neither"];
+		thisQuestionData["neither"] = neither.toString() + "%";
+
+		var disagree = neither + thisQuestionData["disagree"];
+		thisQuestionData["disagree"] = disagree.toString() + "%";
+
+		var disagree_strongly = disagree + thisQuestionData["disagree_strongly"];
+		thisQuestionData["disagree_strongly"] = disagree_strongly.toString() + "%";
+
+		var unsure = disagree_strongly + thisQuestionData["unsure"];
+		thisQuestionData["unsure"] = unsure.toString() + "%";
+
+		var na = unsure + thisQuestionData["na"];
+		thisQuestionData["na"] = na.toString() + "%";
+
+		// use the calculated widths to adjust the rectangles
+		$.each(thisQuestionData, function(indexa, responseCategorya){
+			var selector = " .response-endline." + indexa;
+			d3.select(questionDivId).select(selector).transition().attr("width", responseCategorya);
+		});
 
 	});
 
 	d3.selectAll(".response-bar").on("mouseover", function(d){
 		var tooltipText = $(this).attr("data-percentage");
-		$('#tooltip').append(tooltipText);       
-	}).on("mouseout", function(){ 
+		$('#tooltip').append(tooltipText);
+	}).on("mouseout", function(){
         $('#tooltip').empty();
     });
-	$(".response-bar").mouseover(function(e) {        
+	$(".response-bar").mouseover(function(e) {
         //Set the X and Y axis of the tooltip
         $('#tooltip').css('top', e.pageY  );
-        $('#tooltip').css('left', e.pageX + 20 );         
-    }).mousemove(function(e) {    
+        $('#tooltip').css('left', e.pageX + 20 );
+    }).mousemove(function(e) {
         //Keep changing the X and Y axis for the tooltip, thus, the tooltip move along with the mouse
-        $("#tooltip").css({top:(e.pageY)+"px",left:(e.pageX+20)+"px"});        
+        $("#tooltip").css({top:(e.pageY)+"px",left:(e.pageX+20)+"px"});
     });
 
     $(".loader").fadeOut(400);
 }
 
 
-
-
-// function generateGraphs(){
-//   var admin4List = [];
-//   $.each(displayedShelters, function(index, shelter){
-//     if($.inArray(shelter.properties.admin4, admin4List) === -1){
-//       admin4List.push(shelter.properties.admin4);
-//     }
-//   });
-//   var maleYoung = 0;
-//   var maleChild = 0;
-//   var maleAdult = 0;
-//   var maleSenior = 0;
-//   var femaleYoung = 0;
-//   var femaleChild = 0;
-//   var femaleAdult = 0;
-//   var femaleSenior = 0;
-//   var pregnant = 0;
-//   var mSingleHead = 0;
-//   var fSingleHead = 0;
-//   var mDisabled = 0;
-//   var fDisabled = 0;
-//   $.each(demographicData, function(index, household){
-//     if($.inArray(household.admin4, admin4List) !== -1){
-//       maleYoung += parseInt(household.male_young, 10);
-//       maleChild += parseInt(household.male_child, 10);
-//       maleAdult += parseInt(household.male_adult, 10);
-//       maleSenior += parseInt(household.male_senior, 10);
-//       femaleYoung += parseInt(household.female_young, 10);
-//       femaleChild += parseInt(household.female_child, 10);
-//       femaleAdult += parseInt(household.female_adult, 10);
-//       femaleSenior += parseInt(household.female_senior, 10); 
-//       pregnant += parseInt(household["vulnerabilities/pregnant_lactating"], 10);     
-//       mDisabled += parseInt(household["vulnerabilities/disabled_ill_male"], 10);
-//       fDisabled += parseInt(household["vulnerabilities/disabled_ill_female"], 10);
-//       if(household["vulnerabilities/single_headed"] === "yes"){
-//         switch(household["vulnerabilities/single_headed_sex"]){
-//           case 'male':
-//             mSingleHead += 1;
-//             break;
-//           case 'female':
-//             fSingleHead += 1;
-//             break;
-//         }
-//       }
-//     }
-//   });
-//   var femaleTotal = femaleYoung + femaleChild + femaleAdult + femaleSenior;
-//   var maleTotal = maleYoung + maleChild + maleAdult + maleSenior;
-//   var grandTotal = femaleTotal + maleTotal;
-//   $("#maleYoung").html(formatCommas(maleYoung));
-//   $("#maleChild").html(formatCommas(maleChild));
-//   $("#maleAdult").html(formatCommas(maleAdult));
-//   $("#maleSenior").html(formatCommas(maleSenior));
-//   $("#femaleYoung").html(formatCommas(femaleYoung));
-//   $("#femaleChild").html(formatCommas(femaleChild));
-//   $("#femaleAdult").html(formatCommas(femaleAdult));
-//   $("#femaleSenior").html(formatCommas(femaleSenior));
-//   $("#pregnant").html(formatCommas(pregnant));
-//   $("#mSingleHead").html(formatCommas(mSingleHead));
-//   $("#fSingleHead").html(formatCommas(fSingleHead));
-//   $("#mDisabled").html(formatCommas(mDisabled));
-//   $("#fDisabled").html(formatCommas(fDisabled));
-//   $("#mSingleHead").html(formatCommas(mSingleHead));
-//   $("#fSingleHead").html(formatCommas(fSingleHead));
-//   $("#maleTotal").html(formatCommas(maleTotal));
-//   $("#femaleTotal").html(formatCommas(femaleTotal));
-//   $("#grandTotal").html(formatCommas(grandTotal));
-// }
-
 //start function chain for map
-getData();
+getDataBaseline();
